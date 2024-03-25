55bb89d28be68f4b277e0ca13a3f158a2075d61d
hajikazuo <hajikazuo@outlook.com>
2024-03-25 10:37:25 -0300

    feat/aula2

--- BlazorShop.Api/Program.cs
+++ BlazorShop.Api/Program.cs
@@ -1,4 +1,6 @@
 using BlazorShop.Api.Context;
+using BlazorShop.Api.Repositories;
+using BlazorShop.Api.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
 var app = builder.Build();
 
---
+++ BlazorShop.Api/Repositories/ProdutoRepository.cs
@@ -0,0 +1,38 @@
+﻿using BlazorShop.Api.Context;
+using BlazorShop.Api.Entities;
+using BlazorShop.Api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorShop.Api.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Produto> GetItem(int id)
+        {
+            var produto = await _context.Produtos.Include(c => c.Categoria).SingleOrDefaultAsync(c => c.Id == id);
+
+            return produto;
+        }
+
+        public async Task<IEnumerable<Produto>> GetItens()
+        {
+            var produtos = await _context.Produtos.Include(c => c.Categoria).ToListAsync();
+
+            return produtos;
+        }
+
+        public async Task<IEnumerable<Produto>> GetItensPorCategoria(int id)
+        {
+            var produto = await _context.Produtos.Include(c => c.Categoria).Where(c => c.CategoriaId == id).ToListAsync();
+
+            return produto;
+        }
+    }
+}
